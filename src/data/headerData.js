041ea376e67f8b0c0863642aab7972b516f8a222
Fatima Zahra <122480841+fatimazahra199@users.noreply.github.com>
2023-05-28 13:15:24 +0000

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -5,7 +5,7 @@ export const headerData = {
   name: "Fatima Zahra Faryat",
   title: "Full-stack  Developer",
   desciption:
-    "Life may be a meandering path but don't worry about it, rather enjoy it. It is about the journey, not the destination so explore all that comes your way, learn and grow from it, for this journey only happens once.",
+    "The only way to do great work is to love what you do.",
   image: profile,
   resumePdf: resume,
 };
